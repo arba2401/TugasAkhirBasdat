c5d6d5d72cb78ca38f6a3af394bdc5a78b711f97
MRafliAditya <rafli14aditya@gmail.com>
2021-12-21 12:26:00 +0700

    tambah index

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db=pg_connect('host=localhost dbname=tugasakhirbasdat user=postgres password=iostreamaja');
+$db=pg_connect('host=localhost dbname=tugasakhirbasdat user=postgres password=rafli123');
 if( !$db ){
     die("Gagal terhubung dengan database: " . pg_connect_error());
 }
